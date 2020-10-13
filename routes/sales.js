fa3cb7557945b3e38c7a2f69bf7951a584a18ba9
Cole Williamson <williamsoncole@gmail.com>
2020-10-13 12:29:09 -0500

    Added ability to enter multiple products into each invoice on quickbooks.

--- routes/sales.js
+++ routes/sales.js
@@ -6,18 +6,38 @@ const router = express.Router();
 
 // Attempts to enter invoice into quickbooks, if this succeeds it enters into postgres.
 router.post('/invoice', async (req, res) => {
-  const {
-    customer,
-    salesperson,
-    product,
-    price,
-    quantity,
-    serviceDate,
-  } = req.body.invoice;
-  const totalPrice = price * quantity;
+  const { customer, salesperson, items } = req.body.invoice;
+  let totalPrice = 0;
+
+  items.forEach((element) => {
+    totalPrice += element.product.price * element.quantity;
+  });
+
+  console.log(totalPrice);
+
+  const line = items.map((item) => {
+    const { quantity, price } = item;
+    const total = item.price * item.quantity;
+    return {
+      DetailType: 'SalesItemLineDetail',
+      // Amount: totalPrice,
+      Amount: total,
+      SalesItemLineDetail: {
+        // Qty: quantity,
+        // UnitPrice: price,
+        Qty: quantity,
+        UnitPrice: price,
+        ItemRef: {
+          name: 'Services',
+          value: '1',
+        },
+      },
+    };
+  });
+
   let success = null;
   const token =
-    'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..f0SwEPeAQaXTVEpmeZ7BhA.xpaDitfV-e5v7doi9eEKNP_w8iUpGt5xNAcFapkXeGrjo3YiBN2MDzc6mhit-6U5NmwrRG-5SbcBWF2F9oFcN-p_W2tVxT2prSiPOcnEoNRSoaV4H7b06EhIFG-J9VWn-81QtxrOOaNW0pK98f2f1n65Nlwr4ASmRbOOMATzOPssskpghXIzjqMmCUpc6sLmkpUe7zfAR11FCaXRmuMTGGDGmpCKpekW6NfcQNqxc85RjobgDA5Kb77rghfG9MSqKUJRRiJMLEq-tsIHv5nDaOXYs06opovAxterT9fqM-l74AV1Spzc1X8EAOGD5MXC5T_avncEFuGtzqK1lyQ1VVgXoBjpiSncGrKSWSBifZdLEgg6CTMC9AO6GJcdg3-_0cLSl_chTFJJ-LytUOtZv-lVr98EDImTj6LlZ-8DgMaVUiR3hNEm-eXHMpH3YZkHdS1wq-twbMNNiAQAuOMPw_Z1s39Tgix194OQEoHVgHfBgEPgcFKR3ZesJzGs3l0iiLF2H-zjzSsR_ZEKzqRt2TrWJD1NN1ePbTGxS38blU1Iyh7ijvef_gtZP7o9qnDXg51E7-BBHuAm0GAPzTL2HojJgrJMFJ7BI8m4ngsOzFzSxZy55T95OAh3_UTtApRGTj04gSsWAY3iKPaH9N8chLZ5QLYgDpoowI4sCHUyhJN4xOLg8QAWyI4LavPxjK9Bb2yF6ZB8K2pcGEKv4RUbsQPTqls15rkRcGXmqzZOpJquTk1Am88BpiiW4zCNe7r0vkZVReqoTiUgMe4NwIuOQ0jLAfHuncP8sz9irHYE6viJgumqgwib5k0g8evpET8I5hKvrfcluD34VgweT79SUw.0iO2pdvq3hfpUqY05KIMpQ';
+    'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..0b9TgC5aanhzYYnP77cEJg.valFZiPWlp8J-SxjkNRSsz3IfAM-oeS3TxVZ3WEZTZ8i2PEWYlUXkWogIkcvuW70LRCKSsshJLWOtk5X0rnTPSqpJUinRVRRHfjOYNxPeC573xIMiLv0DyR0DH9gxv5pQPryQQeKnHp6z4D5Zhtm0sdlslQJa39vx41Uasuu7ls95yOzo0C4p9eS6sa8FFap8AzkBnQnO7OL0VFsp6w_nj-0TgRwrK4o08QwcyhHHZbusLqA-1eFKNwJV9dqM0MRO1Dx3vGZv9SF6dkcTo1ekX9VD16wKwGYSC-h6A1KaDxjvFcMvZHUrTp8pRwJehTI8S5VauKh-xzIyh7CI4LjuD6hbFcAz43vx6OhJ6HCxMaulYiLM6CN5JMC9AEe-e-eGbIHU0L2qt-lE1ojvF6D2MyIyQ19OA21_ebtLkduPZoNZw6TAPcox2f1SlZKelBJSc9nGqb2JhvxTLl7ObFjV3Zf9wivfBCFhfOWweVNXvezU94ngj7Uc5BFkj8N0cXq1wh6JbeKeJ-omXhSuhGc8qG5lTI9wk8DIWQpdBhrrcHAtEqKzyQQXAhMdVhV3CZpGrqe7Os6d_pPzgE5y5X9OcMJ_YobUP89FhunGR_-peSqEnicvSswr1Qw8fwrDBxu_Rucsvnr-HeLjTW3jlOlRCKIV5ZXq5NA-cAgWwozcECAYLmR2xc-HT38bxyxxP2cKorlAHvjzIGEsiy8XcGZhlhZeCoayYsYbL250Az9i93lk3QpXGj2yrcYF-flVb-kXnoDfCRirUBWMaZHSiYxI1FclxomdBiWCo-NS-iyt02y59z39CA4IUJOal7Qz5hpwfwYUJj0wwzfbBGQHNwlzg.UJRfaCxbZgfDNc0q8WZOQw';
   await axios({
     method: 'post',
     url:
@@ -26,20 +46,7 @@ router.post('/invoice', async (req, res) => {
       Authorization: `Bearer ${token}`,
     },
     data: {
-      Line: [
-        {
-          DetailType: 'SalesItemLineDetail',
-          Amount: totalPrice,
-          SalesItemLineDetail: {
-            Qty: quantity,
-            UnitPrice: price,
-            ItemRef: {
-              name: 'Services',
-              value: '1',
-            },
-          },
-        },
-      ],
+      Line: line,
       CustomerRef: {
         // need to change to customer id
         value: '1',
