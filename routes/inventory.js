4f382c66d91ee64577f465cfde11c0a13e4c7e05
Cole Williamson <williamsoncole@gmail.com>
2020-11-08 17:02:59 -0600

    Inventory_forecast only returns one month.

--- routes/inventory.js
+++ routes/inventory.js
@@ -7,18 +7,26 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   const currentYear = format(new Date(), 'yyyy');
   const currentMonth = format(new Date(), 'MM');
-  const { monthsAhead } = req.body;
+  // const { monthsAhead } = req.body;
+  const { month } = req.body;
   const blah = '';
 
   console.log(req.body);
 
+  // const inventory_forecast = await pool.query(
+  //   `SELECT * FROM inventory_forecast
+  //   JOIN products on inventory_forecast.filter_id = products.id
+  //     WHERE sale_date < date '${currentYear}-${currentMonth}-01' + interval '${
+  //     monthsAhead + 1
+  //   } months'
+  //     AND sale_date > date '${currentYear}-${currentMonth}-01' + interval '${monthsAhead} months'`
+  // );
+  // res.send(inventory_forecast.rows);
+
   const inventory_forecast = await pool.query(
     `SELECT * FROM inventory_forecast
     JOIN products on inventory_forecast.filter_id = products.id
-      WHERE sale_date < date '${currentYear}-${currentMonth}-01' + interval '${
-      monthsAhead + 1
-    } months'
-      AND sale_date > date '${currentYear}-${currentMonth}-01' + interval '${monthsAhead} months'`
+      WHERE EXTRACT (MONTH FROM sale_date) = ${month}`
   );
   res.send(inventory_forecast.rows);
 });
